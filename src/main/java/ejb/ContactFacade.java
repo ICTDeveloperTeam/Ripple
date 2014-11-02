9d82cc429a9ea7cc02664574bb09349ba6e88dde
YusukeKato <YusukeKato@192.168.1.16>
2014-11-02 22:40:00 +0900

    JAX-RS変更

--- src/main/java/ejb/ContactFacade.java
+++ src/main/java/ejb/ContactFacade.java
@@ -1,8 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package ejb;
 
 import entities.Contact;
@@ -23,45 +23,58 @@ public class ContactFacade extends AbstractFacade<Contact> {
     @PersistenceContext(unitName = "jp.ac.u-tokai.ictedu.hacku14.ripple_Ripple_war_1.0-SNAPSHOTPU")
     private EntityManager em;
     Query query;
-
+    
     UserMaster um;
     List<UserMaster> umList;
     List<Contact> contact;
-    
     
     @Override
     protected EntityManager getEntityManager() {
         return em;
     }
-
+    
     public ContactFacade() {
         super(Contact.class);
     }
     
     public List<UserMaster> getContactInfo(String twitterID){
         query = em.createNamedQuery("UserMaster.findByTwitterID").setParameter("twitterID", twitterID);//UserMasterからUser情報をすべてとってくるクエリを飛ばしている
-        um=(UserMaster)query.getSingleResult();
-        query = em.createNamedQuery("Contact.findByUserID").setParameter("userID", um.getUserID());
-        contact=query.getResultList();
-        for(Contact cont : contact){
-            query = em.createNamedQuery("UserMaster.findByUserID").setParameter("userID", cont.getReceiveUID());
+        if(!query.getResultList().isEmpty()){
             um=(UserMaster)query.getSingleResult();
-            umList.add(um);
+            query = em.createNamedQuery("Contact.findByUserID").setParameter("userID", um.getUserID());
+            if(!query.getResultList().isEmpty()){
+                contact=query.getResultList();
+                for(Contact cont : contact){
+                    query = em.createNamedQuery("UserMaster.findByUserID").setParameter("userID", cont.getReceiveUID());
+                    if(!query.getResultList().isEmpty()){
+                        um=(UserMaster)query.getSingleResult();
+                        umList.add(um);
+                    }
+                }
+                return umList;
+            }
         }
-        return umList;
+        return null;
     }
     
     public List<UserMaster>resContactInfo(String twitterID){
         query = em.createNamedQuery("UserMaster.findByTwitterID").setParameter("twitterID", twitterID);//UserMasterからUser情報をすべてとってくるクエリを飛ばしている
-        um=(UserMaster)query.getSingleResult();
-        query = em.createNamedQuery("Contact.findByUserID").setParameter("receiveUID", um.getUserID());
-        contact=query.getResultList();
-        for(Contact cont : contact){
-            query = em.createNamedQuery("UserMaster.findByUserID").setParameter("userID", cont.getUserID());
+        if(!query.getResultList().isEmpty()){
             um=(UserMaster)query.getSingleResult();
-            umList.add(um);
+            query = em.createNamedQuery("Contact.findByUserID").setParameter("receiveUID", um.getUserID());
+            if(!query.getResultList().isEmpty()){
+                contact=query.getResultList();
+                for(Contact cont : contact){
+                    query = em.createNamedQuery("UserMaster.findByUserID").setParameter("userID", cont.getUserID());
+                    if(!query.getResultList().isEmpty()){
+                        um=(UserMaster)query.getSingleResult();
+                        umList.add(um);
+                    }
+                }
+                return umList;
+            }
         }
-        return umList;
+        return null;
     }
     
 }
